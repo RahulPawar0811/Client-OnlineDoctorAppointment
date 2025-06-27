c32971ed836d26b259bcbb0127f5bda534759e2e
Rahul Ganesh Pawar <rahulpawar9887@gmail.com>
2025-06-28 04:12:01 +0530

    adding url in frontend

--- src/components/apiConfig.js
+++ src/components/apiConfig.js
@@ -1,8 +1,10 @@
 // apiConfig.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
-const imageUrl = 'http://localhost:5000';
+//const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://server-online-doctor-appointment.vercel.app';
+const imageUrl = 'https://server-online-doctor-appointment.vercel.app';
+//const imageUrl = 'http://localhost:5000';
 
 // Create an axios instance with default config
 const api = axios.create({
